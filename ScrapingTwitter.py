870f47c616abf9bab1a833282cbf71a736f81aff
Curry-pad <75872389+Curry-pad@users.noreply.github.com>
2025-04-28 16:02:54 +0900

    Update ScrapingTwitter.py
    ＼(^o^)／

--- ScrapingTwitter.py
+++ ScrapingTwitter.py
@@ -10,7 +10,11 @@ import json
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    print("実処理なし")
+    return {
+        "code" : 200,
+        "message": "Hello World"
+    }
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
