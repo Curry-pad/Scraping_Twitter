b59986e345294db085c466846d5df1d6d9da9338
Curry-pad <75872389+Curry-pad@users.noreply.github.com>
2025-04-28 23:16:22 +0900

    Update ST_Following.py

--- ST_Following.py
+++ ST_Following.py
@@ -41,7 +41,7 @@ def Following(
     #ステータスコードが200番台ではないのにexceptに飛んでくれないことがある、その対策
     #検知条件：ステータスコードを100で割ったときの商が2でない
     if response.status_code // 100 != 2:
-      raise requests.exceptions.RequestException("Request Failed for Twitter API return code " + response.status_code)
+      raise requests.exceptions.RequestException("Request Failed for Twitter API return code " + str(response.status_code))
   
     # レスポンスのステータスコードを表示
     print('ステータスコード:', response.status_code)
