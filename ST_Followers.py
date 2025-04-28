f36ab05192ceb28d128b277f69d0bb9ea6554358
Curry-pad <75872389+Curry-pad@users.noreply.github.com>
2025-04-28 23:35:23 +0900

    Update ST_Followers.py

--- ST_Followers.py
+++ ST_Followers.py
@@ -46,7 +46,7 @@ def Followers(
     if response.status_code // 100 != 2:
       return {
         "code" : response.status_code,
-        "Request Failed for Twitter API return code " + str(response.status_code)
+        "message" : "Request Failed for Twitter API return code " + str(response.status_code)
       }
 
     #レスポンスをjson形式に変換
