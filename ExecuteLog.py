e37cff45018fd5b24270952a505497cff14359a6
Curry-pad <75872389+Curry-pad@users.noreply.github.com>
2025-04-29 18:41:03 +0900

    Update ExecuteLog.py

--- ExecuteLog.py
+++ ExecuteLog.py
@@ -1,9 +1,17 @@
 
 def Write_ExecuteLog(write_text):
 
+  import os
+  
+  #フォルダがない場合は作る
+  os.mkdir('./Log')
+
   #ファイルがない場合は作る
   #ファイルをフォルダの中に作りたい
   
   #ファイル名：Log_yyyymmdd.log
+  filename = str(datetime.date.today()) + ".log"
   
+  with open(filename, "w") as o:
+    print(write_text, file=o)
   
