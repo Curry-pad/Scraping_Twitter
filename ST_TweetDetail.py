a0ee63f970c893d6493f698b2aeb9ebf6341831d
Curry-pad <75872389+Curry-pad@users.noreply.github.com>
2025-05-03 18:14:31 +0900

    Update ST_TweetDetail.py

--- ST_TweetDetail.py
+++ ST_TweetDetail.py
@@ -42,7 +42,7 @@ def TweetDetail(
     if response.status_code // 100 != 2:
       return {
         "code" : response.status_code,
-        "message" : "Request Failed for Twitter API returned code " + str(response.status_code)
+        "message" : "Request Failed for Scraping Twitter by Python returned code " + str(response.status_code)
       }
 
     #レスポンスをjson形式に変換
