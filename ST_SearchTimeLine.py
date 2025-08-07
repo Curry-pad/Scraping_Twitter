6819b163dfe250fa1f8fa5d17c77eb1c3d33b359
Curry-pad <75872389+Curry-pad@users.noreply.github.com>
2025-08-07 23:17:18 +0900

    ST_SearchTimeLine.py を更新

--- ST_SearchTimeLine.py
+++ ST_SearchTimeLine.py
@@ -53,7 +53,7 @@ def SearchTimeLine(
     if response.status_code // 100 != 2:
       return {
         "code" : response.status_code,
-        "message" : CommonFunction.Create_HTTPErrorMessage(response.status_code)
+        "message" : CommonFunction.Create_HTTPErrorMessage(response.status_code,response.text)
       }
 
     #レスポンスをjson形式に変換
