e6de7b98c3d3d707e273fa925f991db1c8cafe51
Curry-pad <75872389+Curry-pad@users.noreply.github.com>
2025-08-07 23:16:09 +0900

    ST_Followers.py を更新

--- ST_Followers.py
+++ ST_Followers.py
@@ -56,7 +56,7 @@ def Followers(
     if response.status_code // 100 != 2:
       return {
         "code" : response.status_code,
-        "message" : CommonFunction.Create_HTTPErrorMessage(response.status_code)
+        "message" : CommonFunction.Create_HTTPErrorMessage(response.status_code,response.text)
       }
 
     #レスポンスをjson形式に変換
