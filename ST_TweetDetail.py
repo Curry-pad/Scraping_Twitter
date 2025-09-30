fb9faf060e8d0dfb909e381739bf75d75c5f9d39
Curry-pad <75872389+Curry-pad@users.noreply.github.com>
2025-09-30 15:00:32 +0900

    Update ST_TweetDetail.py

--- ST_TweetDetail.py
+++ ST_TweetDetail.py
@@ -70,8 +70,8 @@ def TweetDetail(
 
     print('文字コード（修正前）：', response.encoding)
     #response.encoding = response.apparent_encoding #これを使うとなぜかNoneになってしまう
-    #response.encoding = 'utf-8'
-    response.encoding = 'SHIFT_JIS'
+    response.encoding = 'utf-8'
+    #response.encoding = 'SHIFT_JIS'
     print('文字コード：（修正後）', response.encoding)
   
     # レスポンスのステータスコードを表示
