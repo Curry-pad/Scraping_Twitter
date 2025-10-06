d1b5e1a032471e97d0ae1071e51b98ad00c1550a
Curry-pad <75872389+Curry-pad@users.noreply.github.com>
2025-10-06 10:37:34 +0900

    Update Get_XClientTransactionId.py

--- Get_XClientTransactionId.py
+++ Get_XClientTransactionId.py
@@ -3,3 +3,31 @@ def Get_XClientTransactionId(
   endPointURL
 ):
 
+  #まずはTwitterの入り口部分にアクセスして、XCTIの取得に必要な情報（ondemand_file_response）を取得する
+  
+  import bs4
+  import requests
+  from x_client_transaction.utils import generate_headers, handle_x_migration, get_ondemand_file_url
+  
+  # INITIALIZE SESSION
+  session = requests.Session()
+  session.headers = generate_headers()
+  
+  
+  # GET HOME PAGE RESPONSE
+  # required only when hitting twitter.com but not x.com
+  # returns bs4.BeautifulSoup object
+  home_page_response = handle_x_migration(session=session)
+  
+  # for x.com no migration is required, just simply do
+  home_page = session.get(url="https://x.com")
+  home_page_response = bs4.BeautifulSoup(home_page.content, 'html.parser')
+  
+  
+  # GET ondemand.s FILE RESPONSE
+  ondemand_file_url = get_ondemand_file_url(response=home_page_response)
+  ondemand_file = session.get(url=ondemand_file_url)
+  ondemand_file_response = bs4.BeautifulSoup(ondemand_file.content, 'html.parser')
+
+  print(ondemand_file_response)
+
