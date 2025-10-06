94c6346d54d053aee78318ed4fdb10409b73a63e
Curry-pad <75872389+Curry-pad@users.noreply.github.com>
2025-10-06 11:41:15 +0900

    Create TweeterPy.py

---
+++ TweeterPy.py
@@ -0,0 +1,16 @@
+from tweeterpy import TweeterPy
+from tweeterpy.util import find_nested_key
+
+def TweeterPy():
+  
+  # proxy = {'http': 'proxy_here', 'https': 'proxy_here'}
+  proxy = None
+  twitter = TweeterPy(proxies=proxy, log_level="INFO")
+  print(twitter.get_user_id('elonmusk'))
+  print(twitter.get_user_info('elonmusk'))
+  # print(twitter.get_user_data('elonmusk'))
+  # tweets = twitter.get_user_tweets('elonmusk', total=200)
+  # Get all the username from the tweets.
+  # usernames = find_nested_key(tweets,"screen_name")
+  # print(twitter.get_user_media('elonmusk', total=100))
+
