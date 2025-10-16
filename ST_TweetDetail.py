21632f6fa8adfb2fe794e0625bab0958786f1fb3
Curry-pad <75872389+Curry-pad@users.noreply.github.com>
2025-10-16 10:20:08 +0900

    Update ST_TweetDetail.py

--- ST_TweetDetail.py
+++ ST_TweetDetail.py
@@ -11,6 +11,7 @@ def TweetDetail(
   import chardet
   from charset_normalizer import detect
   from bs4 import BeautifulSoup
+  import Get_XClientTransactionId
   
   #query_id = "_8aYOgEDz35BrBcBal1-_w"
   #2025/05/04　クエリID変更対応
@@ -32,11 +33,16 @@ def TweetDetail(
   #query_str = "%22%2C%22referrer%22%3A%22me%22%2C%22controller_data%22%3A%22DAACDAABDAABCgABAAAAAAAAAAAKAAkLMoELhRSgAgAAAAA%3D%22%2C%22with_rux_injections%22%3Afalse%2C%22includePromotedContent%22%3Atrue%2C%22withCommunity%22%3Atrue%2C%22withQuickPromoteEligibilityTweetFields%22%3Atrue%2C%22withBirdwatchNotes%22%3Atrue%2C%22withVoice%22%3Atrue%2C%22withV2Timeline%22%3Atrue%7D&features=%7B%22responsive_web_graphql_exclude_directive_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Afalse%2C%22responsive_web_home_pinned_timelines_enabled%22%3Atrue%2C%22creator_subscriptions_tweet_preview_api_enabled%22%3Atrue%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22c9s_tweet_anatomy_moderator_badge_enabled%22%3Atrue%2C%22tweetypie_unmention_optimization_enabled%22%3Atrue%2C%22responsive_web_edit_tweet_api_enabled%22%3Atrue%2C%22graphql_is_translatable_rweb_tweet_is_translatable_enabled%22%3Atrue%2C%22view_counts_everywhere_api_enabled%22%3Atrue%2C%22longform_notetweets_consumption_enabled%22%3Atrue%2C%22responsive_web_twitter_article_tweet_consumption_enabled%22%3Afalse%2C%22tweet_awards_web_tipping_enabled%22%3Afalse%2C%22freedom_of_speech_not_reach_fetch_enabled%22%3Atrue%2C%22standardized_nudges_misinfo%22%3Atrue%2C%22tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled%22%3Atrue%2C%22longform_notetweets_rich_text_read_enabled%22%3Atrue%2C%22longform_notetweets_inline_media_enabled%22%3Atrue%2C%22responsive_web_media_download_video_enabled%22%3Afalse%2C%22responsive_web_enhance_cards_enabled%22%3Afalse%7D&fieldToggles=%7B%22withArticleRichContentState%22%3Afalse%7D"
   
   url = 'https://' + twitter_domain + '/i/api/graphql/' + query_id + '/TweetDetail?variables=%7B%22focalTweetId%22%3A%22' + target_tweet_id + query_str
+
+  #XCTIを生成する。これがうまくいけば、引数のXCTIは不要になる
+  xcti_res = Get_XClientTransactionId.Get_XClientTransactionId(
+    url,"GET"
+  )
   
   headers = {
     'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
     'Cookie': 'auth_token=' + auth_token + '; ct0=' + ct0 + '; ',
-    'x-client-transaction-id': x_client_transaction_id,
+    'x-client-transaction-id': xcti_res["created_XCTI"],
     'x-csrf-token': ct0,
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
     "content-type": "application/json",
@@ -69,7 +75,7 @@ def TweetDetail(
   print('target_tweet_id = ' + target_tweet_id)
   print('ct0 = ' + ct0)
   print('auth_token = ' + auth_token)
-  print('x-client-transaction-id = ' + x_client_transaction_id)
+  print('x-client-transaction-id = ' + xcti_res["created_XCTI"])
   
   try:
 
