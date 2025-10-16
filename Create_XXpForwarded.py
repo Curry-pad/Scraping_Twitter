16c6c6b6352aa612eec7e179e6027f40b3b63771
Curry-pad <75872389+Curry-pad@users.noreply.github.com>
2025-10-16 11:16:56 +0900

    Update Create_XXpForwarded.py

--- Create_XXpForwarded.py
+++ Create_XXpForwarded.py
@@ -1,6 +1,7 @@
 #https://github.com/dsekz/twitter-x-xp-forwarded-for-header
 def Create_XXpForwarded():
 
+  import twitter_xpff
   from twitter_xpff import XPFFHeaderGenerator
 
   base_key = "0e6be1f1e21ffc33590b888fd4dc81b19713e570e805d4e5df80a493c9571a05"
